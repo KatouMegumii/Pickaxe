46cd00d157eaf2d5866d0ea83bb950623e0874b5
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2024-12-16 20:09:08 +0800

    merged src

---
+++ src/main/java/KatouMegumi/pickaxe/Pickaxe.java
@@ -0,0 +1,10 @@
+package KatouMegumi.pickaxe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pickaxe implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/KatouMegumi/pickaxe/client/PickaxeClient.java
@@ -0,0 +1,10 @@
+package KatouMegumi.pickaxe.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PickaxeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
